85bae12d5f454c85d413a134c697701b192e8cd0
Tharinee Munkwamdee <tharinee_fang@hotmail.com>
2023-01-04 17:57:27 +0700

    fix map layout switching tile

--- generate_latlng_on_map/index.js
+++ generate_latlng_on_map/index.js
@@ -90,8 +90,13 @@ satelliteLayer.addEventListener('click', () => {
 });
 const normalLayer = document.getElementById('normal-layout');
 normalLayer.addEventListener('click', () => {
-  L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {
-    maxZoom: 20,
-    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+  // L.tileLayer('https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png', {
+  //   maxZoom: 20,
+  //   attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+  // }).addTo(map);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 })
